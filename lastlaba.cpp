2229455c9c618a2f1251c8b2b24de4e3fa303277
Quenassier <112952435+Quenassier@users.noreply.github.com>
2023-06-12 22:53:25 +0700

    Update lastlaba.cpp

--- lastlaba.cpp
+++ lastlaba.cpp
@@ -1,209 +1,147 @@
-#include <iostream>
-#include <fstream>
+#include <iostream> // Подключение библиотеки для стандартного ввода-вывода
+#include <fstream> // Подключение библиотеки для работы с файлами
+#include <vector> // Подключение библиотеки для работы с векторами
+#include <map> // Подключение библиотеки для работы с отображениями (map)
+#include <iomanip> // Подключение библиотеки для управления выводом
+#include <string> // Подключение библиотеки для работы со строками
 
+using namespace std; // Использование пространства имен std (стандартной библиотеки)
 
-using namespace std;
-
-
-
-class HomeAccounting {
+class ExpenseTracker { // Определение класса ExpenseTracker
+private:
+    std::string expensesFile; // Путь к файлу для записи расходов
+    std::string incomeFile; // Путь к файлу для записи доходов
+    std::map<std::string, double> expenseCategories; // Категории расходов и суммы расходов по каждой категории
+    double totalExpenses; // Общая сумма расходов
+    double totalIncome; // Общая сумма доходов
 
 public:
-    int other = 0;
+    ExpenseTracker(const std::string& expensesFile, const std::string& incomeFile) // Конструктор класса ExpenseTracker
+        : expensesFile(expensesFile), incomeFile(incomeFile), totalExpenses(0.0), totalIncome(0.0) {} // Инициализация полей класса
 
-    void recordExpense()
-    {
-        long  int expenditure;
-        cout << "vvedite trati: ";
-        cin >> expenditure;
-        this->other = expenditure;
-        ofstream fout("expenses.txt", ios::app);
-
-        if (fout.is_open())
-        {
-            fout << expenditure << endl;
-            fout.close();
+    void recordExpense(const std::string& category, double amount) { // Метод для записи расхода
+        std::ofstream file(expensesFile, std::ios::app); // Создание объекта ofstream для записи в файл с опцией дополнения (append)
+        if (file.is_open()) { // Проверка, удалось ли открыть файл для записи
+            file << category << "," << amount << std::endl; // Запись категории и суммы расхода в файл
+            file.close(); // Закрытие файла
+            totalExpenses += amount; // Увеличение общей суммы расходов
+            expenseCategories[category] += amount; // Увеличение суммы расходов в указанной категории
+            std::cout << "Expense recorded successfully." << std::endl; // Вывод сообщения об успешной записи расхода
         }
-        else
-        {
-            cout << "Unable to open file!" << "\n";
-        }
-    }
-    void recordIncome()
-    {
-        long int summ;
-        cout << "vvedite summ: ";
-        cin >> summ;
-
-        ofstream fout("incomes.txt", ios::app);
-
-        if (fout.is_open())
-        {
-            fout << summ << endl;
-            fout.close();
-        }
-        else
-        {
-            cout << "Unable to open file!" << "\n";
+        else {
+            std::cout << "Unable to open expenses file." << std::endl; // Вывод сообщения об ошибке при открытии файла для записи расхода
         }
     }
 
-    int calculateBalance()
-    {
-        long int incomes = 0;
-        long int expenes = 0;
-
-
-        ifstream fin("incomes.txt");
-
-        if (fin.is_open())
-        {
-            int token;
-
-            while (fin >> token)
-            {
-                incomes += token;
-
-            }
-            fin.close();
+    void recordIncome(double amount) { // Метод для записи дохода
+        std::ofstream file(incomeFile, std::ios::app); // Создание объекта ofstream для записи в файл с опцией дополнения (append)
+        if (file.is_open()) { // Проверка, удалось ли открыть файл для записи
+            file << amount << std::endl; // Запись суммы дохода в файл
+            file.close(); // Закрытие файла
+            totalIncome += amount; // Увеличение общей суммы доходов
+            std::cout << "Income recorded successfully." << std::endl; // Вывод сообщения об успешной записи дохода
         }
-        else
-        {
-            cout << "Unable to open file!" << "\n";
+        else {
+            std::cout << "Unable to open income file." << std::endl; // Вывод сообщения об ошибке при открытии файла для записи дохода
         }
-
-        ifstream fen("expenses.txt");
-
-        if (fen.is_open())
-        {
-            int token;
-            while (fen >> token)
-            {
-                expenes += token;
-            }
-            fen.close();
-        }
-        else
-        {
-            cout << "Unable to open file!" << "\n";
-        }
-        cout << "balance" << "\t";
-        cout << incomes - expenes << "\n";
-
-        return expenes;
-    }
-    void percentmonth() {
-
-
-        int sumfood, sumcloths, sumelectronic, sumperevod;
-        int oct = this->other;
-
-        cout << this->other << " trati za month " << "\n";
-        while (true) {
-            cout << "sumfood ";
-            cin >> sumfood;
-            if (sumfood >= 0 and sumfood <= oct) {
-                oct -= sumfood;
-                break;
-            }
-
-        }
-
-        while (true) {
-            cout << "sumcloths ";
-            cin >> sumcloths;
-            if (sumcloths >= 0 and sumcloths <= oct) {
-                oct -= sumcloths;
-                break;
-            }
-        }
-
-        while (true) {
-            cout << "sumelectronic ";
-            cin >> sumelectronic;
-            if (sumelectronic >= 0 and sumelectronic <= oct) {
-                oct -= sumelectronic;
-                break;
-            }
-        }
-
-        while (true) {
-            cout << "sumperevod ";
-            cin >> sumperevod;
-            if (sumperevod >= 0 and sumperevod <= oct) {
-                oct -= sumperevod;
-                break;
-            }
-        }
-
-        cout << "persent month " << "\n";
-        cout << sumfood * 100 / this->other << "% food" << "\t";
-        cout << sumcloths * 100 / this->other << "% cloths " << "\t";
-        cout << sumelectronic * 100 / this->other << "% electronic " << "\t";
-        cout << sumperevod * 100 / this->other << "% perevod " << "\t";
-        cout << oct * 100 / this->other << "% other expense" << "\n";
-
-
-    }
-    void percentall() {
-        int expenes;
-        expenes = calculateBalance();
-        int sumfood, sumcloths, sumelectronic, sumperevod;
-        int oct = expenes;
-
-        cout << expenes << " vse trati " << "\n";
-        while (true) {
-            cout << "sumfood ";
-            cin >> sumfood;
-            if (sumfood >= 0 and sumfood <= oct) {
-                oct -= sumfood;
-                break;
-            }
-
-        }
-
-        while (true) {
-            cout << "sumcloths ";
-            cin >> sumcloths;
-            if (sumcloths >= 0 and sumcloths <= oct) {
-                oct -= sumcloths;
-                break;
-            }
-        }
-
-        while (true) {
-            cout << "sumelectronic ";
-            cin >> sumelectronic;
-            if (sumelectronic >= 0 and sumelectronic <= oct) {
-                oct -= sumelectronic;
-                break;
-            }
-        }
-
-        while (true) {
-            cout << "sumperevod ";
-            cin >> sumperevod;
-            if (sumperevod >= 0 and sumperevod <= oct) {
-                oct -= sumperevod;
-                break;
-            }
-        }
-
-        cout << "persent month " << "\n";
-        cout << sumfood * 100 / expenes << "% food" << "\t";
-        cout << sumcloths * 100 / expenes << "% cloths " << "\t";
-        cout << sumelectronic * 100 / expenes << "% electronic " << "\t";
-        cout << sumperevod * 100 / expenes << "% perevod " << "\t";
-        cout << oct * 100 / expenes << "% other expense" << "\n";
-
-
     }
 
+    void calculateBalances() { // Метод для вычисления баланса
+        double balance = totalIncome - totalExpenses; // Вычисление баланса
+        std::cout << "Total Income: " << totalIncome << std::endl; // Вывод общей суммы доходов
+        std::cout << "Total Expenses: " << totalExpenses << std::endl; // Вывод общей суммы расходов
+        std::cout << "Balance: " << balance << std::endl; // Вывод баланса
+    }
+
+    void calculateCategoryPercentagesLastMonth() { // Метод для вычисления процентного соотношения расходов по категориям за прошлый месяц
+        std::map<std::string, double> categoryPercentages; // Категории расходов и их процентные соотношения
+        double lastMonthExpenses = 0.0; // Сумма расходов за прошлый месяц
+        std::ifstream file(expensesFile); // Создание объекта ifstream для чтения файла
+        if (file.is_open()) { // Проверка, удалось ли открыть файл для чтения
+            std::string line; // Переменная для хранения текущей строки
+            while (getline(file, line)) { // Чтение файла построчно
+                // Предполагается, что формат строки следующий: "категория,сумма"
+                size_t pos = line.find(","); // Поиск позиции разделителя (",")
+                std::string category = line.substr(0, pos); // Извлечение категории из строки
+                double amount = stod(line.substr(pos + 1)); // Извлечение суммы расхода из строки и преобразование в тип double
+
+                categoryPercentages[category] += amount; // Увеличение суммы расходов в указанной категории
+                lastMonthExpenses += amount; // Увеличение суммы расходов за прошлый месяц
+            }
+            file.close(); // Закрытие файла
+
+            std::cout << "Category Percentages for Last Month:" << std::endl; // Вывод заголовка для процентного соотношения расходов по категориям за прошлый месяц
+            for (const auto& entry : categoryPercentages) { // Перебор всех категорий и их сумм расходов
+                double percentage = (entry.second / lastMonthExpenses) * 100.0; // Вычисление процентного соотношения
+                std::cout << entry.first << ": " << std::fixed << std::setprecision(2) << percentage << "%" << std::endl; // Вывод категории и процентного соотношения
+            }
+        }
+        else {
+            std::cout << "Unable to open expenses file." << std::endl; // Вывод сообщения об ошибке при открытии файла для чтения расходов
+        }
+    }
+
+    void calculateCategoryPercentagesOverall() { // Метод для вычисления общего процентного соотношения расходов по категориям
+        std::cout << "Category Percentages Overall:" << std::endl; // Вывод заголовка для общего процентного соотношения расходов по категориям
+        for (const auto& entry : expenseCategories) { // Перебор всех категорий и их сумм расходов
+            double percentage = (entry.second / totalExpenses) * 100.0; // Вычисление процентного соотношения
+            std::cout << entry.first << ": " << std::fixed << std::setprecision(2) << percentage << "%" << std::endl; // Вывод категории и процентного соотношения
+        }
+    }
 };
-int main() {
-    HomeAccounting* user1 = new HomeAccounting();
-    user1->recordExpense();
-    user1->recordIncome();
-    user1->percentall();
-    user1->percentmonth();
+
+int main() { // Главная функция программы
+    ExpenseTracker tracker("expenses.txt", "income.txt"); // Создание объекта класса ExpenseTracker с указанием путей к файлам расходов и доходов
+
+    int choice; // Переменная для хранения выбора пользователя
+    do {
+        std::cout << "Expense Tracker Menu" << std::endl; // Вывод меню программы
+        std::cout << "1. Record Expense" << std::endl; // Вывод пункта меню для записи расхода
+        std::cout << "2. Record Income" << std::endl; // Вывод пункта меню для записи дохода
+        std::cout << "3. Calculate Balances" << std::endl; // Вывод пункта меню для вычисления баланса
+        std::cout << "4. Calculate Category Percentages (Last Month)" << std::endl; // Вывод пункта меню для вычисления процентного соотношения расходов по категориям за прошлый месяц
+        std::cout << "5. Calculate Category Percentages (Overall)" << std::endl; // Вывод пункта меню для вычисления общего процентного соотношения расходов по категориям
+        std::cout << "0. Exit" << std::endl; // Вывод пункта меню для выхода
+        std::cout << "Enter your choice: ";
+        std::cin >> choice; // Ввод выбора пользователя
+
+        switch (choice) { // Обработка выбора пользователя
+        case 1: {
+            std::string category; // Переменная для хранения категории расхода
+            double amount; // Переменная для хранения суммы расхода
+            std::cout << "Enter category: ";
+            std::cin >> category; // Ввод категории расхода
+            std::cout << "Enter amount: ";
+            std::cin >> amount; // Ввод суммы расхода
+            tracker.recordExpense(category, amount); // Вызов метода записи расхода
+            break;
+        }
+        case 2: {
+            double amount; // Переменная для хранения суммы дохода
+            std::cout << "Enter amount: ";
+            std::cin >> amount; // Ввод суммы дохода
+            tracker.recordIncome(amount); // Вызов метода записи дохода
+            break;
+        }
+        case 3:
+            tracker.calculateBalances(); // Вызов метода вычисления баланса
+            break;
+        case 4:
+            tracker.calculateCategoryPercentagesLastMonth(); // Вызов метода вычисления процентного соотношения расходов по категориям за прошлый месяц
+            break;
+        case 5:
+            tracker.calculateCategoryPercentagesOverall(); // Вызов метода вычисления общего процентного соотношения расходов по категориям
+            break;
+        case 0:
+            std::cout << "Exiting..." << std::endl; // Вывод сообщения о выходе из программы
+            break;
+        default:
+            std::cout << "Invalid choice. Please try again." << std::endl; // Вывод сообщения о недопустимом выборе
+            break;
+        }
+
+        std::cout << std::endl; // Печать пустой строки для отделения вывода меню от следующей итерации
+    } while (choice != 0); // Выполнять цикл до выбора пункта выхода
+
+    return 0; // Возвращение значения 0, что указывает на успешное завершение программы
 }
